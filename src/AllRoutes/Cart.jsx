2ae2f1f80ff36c76d1da2c78e3aec87ed521572b
prashant1307 <prashantlorgev8@gmail.com>
2022-08-27 23:21:46 +0530

    day-4

--- src/AllRoutes/Cart.jsx
+++ src/AllRoutes/Cart.jsx
@@ -1,5 +1,79 @@
+import { Box, Center, Divider, Flex, Spacer, Text } from "@chakra-ui/react"
+import { Footer } from "../Components/Footer"
+import { Navbar } from "../Components/Navbar"
+import {FiTruck} from "react-icons/fi";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function getCart(){
+    return axios.get("https://asos-server123.herokuapp.com/api/cart")
+}
 export const Cart=()=>{
+const [data,setData]=useState([])
+    useEffect(()=>{
+         getCart().then((res)=>{
+            console.log(res.data)
+            setData(res.data)
+         })
+    },[])
     return (
-        <h1>Cart</h1>
+        <>
+        <Navbar/>
+        <Box paddingTop="30px" bg="rgb(238,238,238)">
+        <Flex margin="auto" w="900px">
+            <Box>
+                
+                    <Flex paddingLeft="20px" paddingRight="20px" marginBottom="20px" paddingTop="20px" paddingBottom="20px" w="510px" bg="white">
+                        <Text fontWeight="600" fontSize="18.4px" color="RGB(45, 45, 45)" fontFamily="Arial">MY BAG</Text>
+                        <Spacer/>
+                        <Text>Items are reserved for 60 minutes</Text> 
+                    </Flex>
+                    {data?.map((item)=>(
+                        <Flex marginBottom="20px" w="510px" paddingBottom="20px" paddingTop="20px" paddingLeft="20px" paddingRight="20px" bg="white">
+                            <Box w="120px">
+                                <img  src={item.image} alt="" />
+                            </Box>
+                            <Spacer/>
+                            <Box paddingLeft="30px" w="360px">
+                                <Text textAlign="left">$ {item.price}</Text>
+                                <Text textAlign="left">{item.title}</Text>
+
+                            </Box>
+                        </Flex>
+                    ))}
+                
+
+
+
+                <Box paddingTop="20px" paddingBottom="20px" paddingLeft="20px" paddingRight="20px" bg="white">
+                    <Flex>
+                        <Box paddingTop="20px" paddingLeft="30px" w="100px">
+                    <FiTruck  size="30px"/>
+                    </Box>
+                    <Box>
+                    <Text textAlign="left">FREE* STANDARD DELIVERY</Text>
+                    <Text textAlign="left">Faster delivery options available to most countries.</Text>
+                    </Box>
+                    </Flex>
+                </Box>
+            </Box>
+            <Spacer/>
+            <Box paddingTop="20px" paddingBottom="20px" paddingLeft="20px" paddingRight="20px"   w="360px" bg="white">
+                <Text textAlign="left" fontWeight="600" fontSize="18.4px" color="RGB(45, 45, 45)" fontFamily="Arial">TOTAL</Text>
+                <Center height='25px'>
+                 <Divider orientation='horizontal' />
+            </Center>
+            <Text textAlign="left" fontWeight="600" fontSize="16.4px" color="RGB(45, 45, 45)" fontFamily="Arial">Sub-total</Text>
+            <Text textAlign="left" fontWeight="600" fontSize="16.4px" color="RGB(45, 45, 45)" fontFamily="Arial">Delivery</Text>
+            <Text marginTop="15px" textAlign="left">Standard Delivery ($ 6.50)</Text>
+            <Center height='25px'>
+                 <Divider orientation='horizontal' />
+            </Center>
+            </Box>
+        </Flex>
+        </Box>
+        <Footer/>
+        </>
     )
 } 
--- src/AllRoutes/ProductPage.jsx
+++ src/AllRoutes/ProductPage.jsx
@@ -2,48 +2,65 @@ import { Footer } from "../Components/Footer"
 import { Navbar } from "../Components/Navbar"
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Box, Flex, Grid, Select, Spacer, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, Select, Spacer, Text } from "@chakra-ui/react";
 function getProducts(params={}){
-   // console.log(params.color)
+    console.log(params.order)
     return axios.get(`https://asos-server123.herokuapp.com/api/products`,{
         params:{
-           
-          //  color:params.color,
-           brand:params.brand
-        //    category:params.category
-        }
+           color:params.color||null,
+           brand:params.brand||null,
+           category:params.category||null,
+           _sort:"price",
+           _order:params.order||null
+        } 
     })
 }
-
+function postCart(item){
+    return axios.post("https://asos-server123.herokuapp.com/api/cart",{
+        title:item.title,
+        brand:item.brand,
+        image:item.image,
+        price:item.price,
+        color:item.color,
+        category:item.category
+    })
+}
 export const ProductPage=()=>{
     const [data,setData]=useState([])
     const [brand,setBrand]=useState("")
     const [category,setCategory]=useState("")
     const [color,setColor]=useState("")
-
+    const [order,setOrder]=useState("")
     useEffect(()=>{
-        handleGetProducts(brand,color,category)
-    },[brand,color,category])
+        handleGetProducts(brand,color,category,order)
+    },[brand,color,category,order])
 
-    function handleGetProducts(brand,color,category){
+
+    
+    function handleGetProducts(brand,color,category,order){
+        console.log(order)
         getProducts({
             brand:brand,
             color:color,
-            category:category
+            category:category,
+            order:order
         })
         .then((res)=>{
             setData(res.data)
         })
     }
-//console.log(color)
+    function handleCart(item){
+      postCart(item)
+    }
+
     return (
         <>
         <Navbar/>
         <Box paddingLeft="50px" marginBottom="30px" paddingRight="50px" paddingTop="30px" paddingBottom="30px" bg="rgb(238,238,238)">
             <Flex marginBottom="20px">
-                <Select border="2px" w="300px" placeholder="Sort">
-                    <option value="">Price high to low</option>
-                    <option value="">Price low to high</option>
+                <Select onChange={(e)=>setOrder(e.target.value)} border="2px" w="300px" placeholder="Sort">
+                    <option value="DESC">Price high to low</option>
+                    <option value="ASC">Price low to high</option>
                 </Select>
                 <Spacer/>
                 <Select onChange={(e)=>setCategory(e.target.value)} border="2px" w="300px" placeholder="Category">
@@ -65,7 +82,7 @@ export const ProductPage=()=>{
             </Flex>
             <Flex>
             
-                <Select onChange={(e)=>setColor(e.target.value)} border="2px "  w="300px" placeholder="Colous">
+                <Select onChange={(e)=>setColor(e.target.value)} border="2px "  w="300px" placeholder="Colour">
                     <option value="white">White</option>
                     <option value="black">Black</option>
                     <option value="green">Green</option>
@@ -81,12 +98,13 @@ export const ProductPage=()=>{
         (
             <Box paddingLeft="60px" paddingRight="60px" key={item.id}>
                 <img src={item.image} alt="" />
-                <Text color="RGB(45, 45, 45)" fontFamily="futura-pt, sans-serif" fontSize="14px" textAlign="left">{item.title}</Text>
+                <Text paddingBottom="15px" color="RGB(45, 45, 45)" fontFamily="futura-pt, sans-serif" fontSize="14px" textAlign="left">{item.title}</Text>
                 <Flex>
                 <Text color="grey" fontWeight="700" fontFamily="futura-pt, sans-serif" fontSize="18px" textAlign="left">$ {item.price}</Text>
                 <Spacer/>
-                <Text color="grey" fontWeight="700" fontFamily="futura-pt, sans-serif" fontSize="18px">{item.brand}</Text>
-                </Flex>
+                {/* <Text color="grey" fontWeight="700" fontFamily="futura-pt, sans-serif" fontSize="18px">add to cart</Text>*/}
+                <Button onClick={()=>handleCart(item)}>Add to cart</Button>
+                </Flex> 
                 
             </Box>
         )
